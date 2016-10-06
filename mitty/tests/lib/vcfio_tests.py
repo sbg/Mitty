bc02a01a385cdaf4e191e140316e48c213662bb8
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2016-10-06 15:19:47 -0400

    Better test message

--- mitty/tests/lib/vcfio_tests.py
+++ mitty/tests/lib/vcfio_tests.py
@@ -6,7 +6,7 @@ import mitty.tests
 
 
 def basic_tests():
-  """Basic vcf loading."""
+  """I/O: Basic vcf loading."""
   for fn in ['g0_s0.vcf', 'g0_s0.vcf.gz']:
     df = vio.read_sample_from_vcf(os.path.join(mitty.tests.example_data_dir, fn), 'g0_s0')
     assert df.columns[1] == 'POS', df.columns
