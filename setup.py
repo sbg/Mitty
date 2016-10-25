5aadc68038bbe36d7f88eccba18056debe0b08e8
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2016-10-25 14:16:47 -0400

    Cleaning up cruft in setup.py

--- setup.py
+++ setup.py
@@ -9,29 +9,7 @@ setup(
   author_email='kaushik.ghose@sbgenomics.com',
   packages=find_packages(include=['mitty*']),
   include_package_data=True,
-  entry_points={
-    # Command line scripts
-    'console_scripts': [
-      # Main programs
-      'mitty = mitty.cli:cli'
-    ],
-    # Register the built in plugins
-    'mitty.plugins.sfs': ['double_exp = mitty.plugins.site_frequency.double_exp'],
-    'mitty.plugins.variants': ['snp = mitty.plugins.variants.snp_plugin',
-                               'delete = mitty.plugins.variants.delete_plugin',
-                               'uniformdel = mitty.plugins.variants.uniform_deletions',
-                               'uniformins = mitty.plugins.variants.uniform_insertions',
-                               'insert = mitty.plugins.variants.insert_plugin',
-                               #'inversion = mitty.plugins.variants.inversion_plugin',
-                               #'low_entropy_insert = mitty.plugins.variants.low_entropy_insert_plugin'
-                               ],
-    'mitty.plugins.variants.hotspot': ['uniform = mitty.plugins.variants.hotspots.uniformhot'],
-    'mitty.plugins.population': ['standard = mitty.plugins.population.standard',
-                                 'vn = mitty.plugins.population.vn'],
-    'mitty.plugins.reads': ['simple_sequential = mitty.plugins.reads.simple_sequential_plugin',
-                            'simple_se = mitty.plugins.reads.simple_se',
-                            'simple_illumina = mitty.plugins.reads.simple_illumina_plugin'],
-  },
+  entry_points={'console_scripts': ['mitty = mitty.cli:cli']},
   install_requires=[
     'sphinx',
     'sphinx_rtd_theme',
