1fdf23f69a0504f091006337bfa258e6ef3e7f86
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2016-11-04 23:50:00 -0400

    Removing non-essential packages from setup
    Some packages like Sphinx are needed for documentation - but may remove that totally in favor of simple markdown files
    
    pandas and tables may be needed for data-analysis, but the plan is to remove and simplify all that code

--- setup.py
+++ setup.py
@@ -11,19 +11,17 @@ setup(
   include_package_data=True,
   entry_points={'console_scripts': ['mitty = mitty.cli:cli']},
   install_requires=[
-    'sphinx',
-    'sphinx_rtd_theme',
     'cython',
     'setuptools>=11.0.0',
     'numpy>=1.9.0',
-    'docopt>=0.6.2',
     'click>=3.3',
     'pysam',
-    'h5py>=2.5.0',
     'matplotlib>=1.3.0',
     'scipy',
-    'pandas>=0.18.1',
-    'tables>=3.2.2'
+#    'pandas>=0.18.1',
+#    'tables>=3.2.2'
+#    'sphinx',
+#    'sphinx_rtd_theme',
     'nose'
   ],
   test_suite='nose.collector',
