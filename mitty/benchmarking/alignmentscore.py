7c14fbd59c3879442f4aca05e3c9be55bf3d528b
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2017-01-31 17:58:56 -0500

    Bugfix: now checking for unmapped reads. Marking unmapped reads as d_max + 1

--- mitty/benchmarking/alignmentscore.py
+++ mitty/benchmarking/alignmentscore.py
@@ -14,23 +14,27 @@ def score_alignment_error(r, ri, max_d=200, strict=False):
   :param ri: readinfo for correct alignment
   :param strict: If True, soft clipped alignments or split alignments are marked incorrect
                  if False alignment to breakpoints gets full score
-  :return:
+  :return: -max_d <= d_err <= max_d + 1
+           d_err = max_d +1 if read is unmapped
   """
   d_err = max_d
-  if strict or ri.cigar[0] == '>':
-    # Both the strict case and the case where the read comes from inside a long insertion are scored
-    # similarly
-    d_err = max(min((r.pos + 1 - ri.pos if r.reference_name == ri.chrom else max_d), max_d), -max_d)
-  else:  # Score read as correct if read is placed at any breakpoint correctly
-    if r.reference_name == ri.chrom:
-      correct_pos = ri.pos
-      for cnt, op in cigar_parser.findall(ri.cigar):
-        if op == '=' or op == 'M' or op == 'X':
-          if abs(r.pos + 1 - correct_pos) < abs(d_err):
-            d_err = r.pos + 1 - correct_pos
-          correct_pos += int(cnt)
-        elif op == 'D':
-          correct_pos += int(cnt)
+  if r.is_unmapped:
+    d_err = max_d + 1
+  else:
+    if strict or ri.cigar[0] == '>':
+      # Both the strict case and the case where the read comes from inside a long insertion are scored
+      # similarly
+      d_err = max(min((r.pos + 1 - ri.pos if r.reference_name == ri.chrom else max_d), max_d), -max_d)
+    else:  # Score read as correct if read is placed at any breakpoint correctly
+      if r.reference_name == ri.chrom:
+        correct_pos = ri.pos
+        for cnt, op in cigar_parser.findall(ri.cigar):
+          if op == '=' or op == 'M' or op == 'X':
+            if abs(r.pos + 1 - correct_pos) < abs(d_err):
+              d_err = r.pos + 1 - correct_pos
+            correct_pos += int(cnt)
+          elif op == 'D':
+            correct_pos += int(cnt)
 
   return d_err
 
