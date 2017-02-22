bab5c5ef8af9cfad81f780870483fc3637b38816
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2017-02-22 07:57:53 -0500

    Feature: d_err now indicates wrong chrom and unmapped separately with values beyond d_max

--- mitty/benchmarking/alignmentscore.py
+++ mitty/benchmarking/alignmentscore.py
@@ -16,27 +16,29 @@ def score_alignment_error(r, ri, max_d=200, strict=False):
   :param ri: readinfo for correct alignment
   :param strict: If True, soft clipped alignments or split alignments are marked incorrect
                  if False alignment to breakpoints gets full score
-  :return: -max_d <= d_err <= max_d + 1
-           d_err = max_d +1 if read is unmapped
+  :return: -max_d <= d_err <= max_d + 2
+           d_err = max_d + 1 if wrong chrom
+           d_err = max_d + 2 if unmapped
   """
   d_err = max_d
   if r.is_unmapped:
+    d_err = max_d + 2
+  elif r.reference_name != ri.chrom:
     d_err = max_d + 1
   else:
     if strict or ri.cigar[0] == '>':
       # Both the strict case and the case where the read comes from inside a long insertion are scored
       # similarly
-      d_err = max(min((r.pos + 1 - ri.pos if r.reference_name == ri.chrom else max_d), max_d), -max_d)
+      d_err = max(min((r.pos + 1 - ri.pos), max_d), -max_d)
     else:  # Score read as correct if read is placed at any breakpoint correctly
-      if r.reference_name == ri.chrom:
-        correct_pos = ri.pos
-        for cnt, op in cigar_parser.findall(ri.cigar):
-          if op == '=' or op == 'M' or op == 'X':
-            if abs(r.pos + 1 - correct_pos) < abs(d_err):
-              d_err = r.pos + 1 - correct_pos
-            correct_pos += int(cnt)
-          elif op == 'D':
-            correct_pos += int(cnt)
+      correct_pos = ri.pos
+      for cnt, op in cigar_parser.findall(ri.cigar):
+        if op == '=' or op == 'M' or op == 'X':
+          if abs(r.pos + 1 - correct_pos) < abs(d_err):
+            d_err = r.pos + 1 - correct_pos
+          correct_pos += int(cnt)
+        elif op == 'D':
+          correct_pos += int(cnt)
 
   return d_err
 
