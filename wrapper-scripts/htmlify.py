05d022d867d3a3c3a43bb2f5cefa97a2850f4d38
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2017-02-27 19:25:08 -0500

    Bugfix: Fixed encoding issue with py3

--- wrapper-scripts/htmlify.py
+++ wrapper-scripts/htmlify.py
@@ -4,10 +4,11 @@ import base64
 html = "<html><head></head><body>"
 
 with open('filelist.txt') as fp:
-  for fn in fp:
+  for fn in (ffn.strip() for ffn in fp):
     with open(fn, "rb") as image_file:
       im_type = os.path.splitext(os.path.basename(fn))[-1][1:]
-      html += '<p><img src="data:image/{};base64,{}" /></p>'.format(im_type, base64.b64encode(image_file.read()))
+      html += '<p align="center"><img src="data:image/{};base64,{}" /></p>'.format(
+        im_type, base64.b64encode(image_file.read()).decode('ascii'))
 
 html += "</body>"
 
