8e6f75fe27207c066775e8e1fd88e34273056943
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2017-08-07 16:48:58 -0400

    enhancement: bam_iter implemented

---
+++ mitty/analysis/bam.py
@@ -0,0 +1,67 @@
+import time
+import logging
+
+import pysam
+
+from mitty.benchmarking.alignmentscore import score_alignment_error, load_qname_sidecar, parse_qname
+
+logger = logging.getLogger(__name__)
+
+
+def is_single_end_bam(bam_fname):
+  bam_fp = pysam.AlignmentFile(bam_fname)
+  r = next(bam_fp, None)
+  return not r.is_paired if r is not None else True  # Empty BAM? Don't care
+
+
+def bam_iter(bam_fname, sidecar_fname, limit=None):
+  """Given a BAM file path return us tuples of paired reads.
+
+  :param bam_fname: BAM file name
+  :param sidecar_fname: long qname overflow file
+  :param limit: If not None limit the number of reads scanned to this count
+  :return: a generator that returns pairs of reads from the file
+  """
+  se_bam = is_single_end_bam(bam_fname)
+  long_qname_table = load_qname_sidecar(sidecar_fname)
+
+  read_dict = {}
+  for n, rd in enumerate( pysam.AlignmentFile( bam_fname ).fetch( until_eof=True )):
+    if n >= limit:
+      break
+    if rd.flag & 0b100100000000: continue  # Skip supplementary or secondary alignments
+    ri = parse_qname(rd.qname, long_qname_table=long_qname_table)[1 if rd.is_read2 else 0]
+    if se_bam:
+      yield (rd, ri, True)
+    else:
+      key = rd.qname[:20]
+      if key not in read_dict:
+        read_dict[key] = [None, None]
+
+      rl = read_dict[key]
+      rl[0 if rd.is_read1 else 1] = (rd, ri, True)
+
+      if all(rl):
+        yield rl
+        del read_dict[key]
+
+
+def derr(r, d_max):
+  pass
+
+"""
+
+`derr ( r, d_max )` - return reads with XD tag filled out with d_err metric
+`discard_ref ( r )` - discard reference reads
+`discard_non_ref ( r )` - discard non-reference reads
+`filter_derr ( r, d_range, d_max )` - given a read iterator filter out reads falling in given d_range
+`filter_v ( r, v_range )` - filter out reads with no variants outside this range
+`categorize ( r, cat_dict )` - given a dictionary of filter functions create categories (or sub-categories)
+`count ( r, result)` - count up all the reads for each category, return in the result dictionary
+`save_to_bam( r, header )` - save reads in separate BAM files with names matching the category names
+`read_fate_plot( result, ax )` - plot a histogram with different category labels based on a result dictionary
+                                 passing multiple results will result in multiple histograms on the same axes
+`xmv ( r, d_max, MQ_max, vlen_max, result )` - Three dimensional alignment analysis histogram bin counts
+`xmv_plot ( result, ax )` - alignment analysis plot. Multiple plots on same axes if
+
+"""
