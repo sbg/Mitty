dd1936425a1f3742ed5b05ed88185a42cde1a060
ozemsbg <ozem.kalay@sbgenomics.com>
2018-11-20 15:45:53 +0300

    Overlappig deletion check for sample-genome and unit test

--- mitty/test/simulation/genome/test_sampledgenome.py
+++ mitty/test/simulation/genome/test_sampledgenome.py
@@ -9,24 +9,53 @@ import mitty.simulation.genome.sampledgenome as sg
 def read_data(outname):
   vcf_pointer = pysam.VariantFile(filename=outname)
   sample = vcf_pointer.header.samples[0]
-  gts = []
+  gts = {}
   for rec in vcf_pointer.fetch():
-    gts.append("|".join([str(ind) for ind in rec.samples["HG"].allele_indices]))
+    gts[rec.pos] = "|".join([(str(ind) if ind is not None else ".") for ind in rec.samples["HG"].allele_indices])
   return sample, gts
 
 
 def test_assign_random_gt():
   """Simulation: sampled-genome"""
   tempfilename = "tempfile.vcf"
-  sg.assign_random_gt(input_vcf=os.path.join(mitty.test.example_data_dir, 'gt_free.vcf'),
-                      output=open(tempfilename, "w"), sample_name="HG", default_af=0.1,
-                      seed=5081973)
 
-  sample_name, gt_array = read_data(tempfilename)
-  os.remove(tempfilename)
+  #gt for overlapping deletion is 0|1
+  sg.assign_random_gt(input_vcf=os.path.join(mitty.test.example_data_dir, 'test-sample-genome-with-af.vcf'),
+                      output=open(tempfilename, "w"), sample_name="HG", default_af=0.1,
+                      seed=1)
+
+  sample_name, gt_map = read_data(tempfilename)
 
   assert sample_name == "HG"
-  assert "0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|1_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|1" \
-         "_1|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_1|0" \
-         "_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|1_0|0_0|0_0|0_0|0_0|0_0|0_1|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0" \
-         "_0|0_1|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|0" == "_".join(gt_array)
+  assert gt_map[30923] == "0|." or gt_map[30923] == "1|."
+
+  # gt for overlapping deletion is 0|0
+  sg.assign_random_gt(input_vcf=os.path.join(mitty.test.example_data_dir, 'test-sample-genome-with-af.vcf'),
+                      output=open(tempfilename, "w"), sample_name="HG", default_af=0.1,
+                      seed=2)
+
+  _, gt_map = read_data(tempfilename)
+  assert gt_map[30923] != ".|."
+
+  # gt for overlapping deletion is 1|1
+  sg.assign_random_gt(input_vcf=os.path.join(mitty.test.example_data_dir, 'test-sample-genome-with-af.vcf'),
+                      output=open(tempfilename, "w"), sample_name="HG", default_af=0.1,
+                      seed=204)
+
+  _, gt_map = read_data(tempfilename)
+  assert gt_map[30923] == ".|."
+
+  # gt for overlapping deletion is 1|0
+  sg.assign_random_gt(input_vcf=os.path.join(mitty.test.example_data_dir, 'test-sample-genome-with-af.vcf'),
+                      output=open(tempfilename, "w"), sample_name="HG", default_af=0.1,
+                      seed=124)
+
+  _, gt_map = read_data(tempfilename)
+  assert gt_map[30923] == ".|0" or gt_map[30923] == ".|1"
+
+  os.remove(tempfilename)
+
+  assert "0|0_0|1_0|0_0|0_0|0_0|0_0|0_0|0_0|0_1|0_0|0_0|0_0|0_1|0_0|0_0|0_0|0_0|1_0|0_0|1_0|0_0|0_0|0_0|0_0|0_0|0_0|0_" \
+         "1|0_.|._1|1_0|0_0|0_.|._0|0_0|0_0|1_0|0_0|0_0|0_1|0_0|0_0|0_0|0_0|0_0|0_0|1_.|._0|0_0|0_0|0_0|0_1|0_0|0_0|0_" \
+         "0|0_0|0_0|0_0|0_0|0_0|0_0|0_0|1_0|0_0|0_0|0_0|0_0|0_0|1_0|0_0|0_1|0_.|0_0|0_0|1_0|0_1|0_1|0_0|0_0|0_0|0_0|0_" \
+         "0|1_0|0_0|0_0|0_0|0_1|1_.|._0|0_1|0_0|0" == "_".join(gt_map.values())
